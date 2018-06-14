64598bb611df64f0f1002b0dd91a6a15defd3412
Corinne Roosen <corinnesmailbox@gmail.com>
2018-06-14 20:46:12 +0200

    Update routes

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,14 @@ const history = createHistory()
 import App from './App'
 import Pii from './components/Course/Pii'
 import Privacy from './components/Course/Privacy'
+import Law from './components/Course/Law'
+import Rights from './components/Course/Rights'
+
+import CaseFacebook from './components/Course/CaseFacebook'
+import CaseStrava from './components/Course/CaseStrava'
+import CaseTinderGrindr from './components/Course/CaseTinderGrindr'
+
+import Results from './components/Course/Results'
 
 
 export default () => {
@@ -18,6 +26,12 @@ export default () => {
         <Route exact path='/' component={App}/>
         <Route path='/course/privacy' component={Privacy}/>
         <Route path='/course/pii' component={Pii}/>
+        <Route path='/course/law' component={Law}/>
+        <Route path='/course/rights' component={Rights}/>
+        <Route path='/course/case-facebook' component={CaseFacebook}/>
+        <Route path='/course/case-tinder-grindr' component={CaseTinderGrindr}/>
+        <Route path='/course/case-strava' component={CaseStrava}/>
+        <Route path='/course/results' component={Results}/>
       </Switch>
      </Router>
   )
