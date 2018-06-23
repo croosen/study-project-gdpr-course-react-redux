beec70aa6fa8a96b010a2c48eac0c266d731a1e6
Corinne Roosen <corinnesmailbox@gmail.com>
2018-06-23 21:53:36 +0200

    Add header bar

--- src/Frame.tsx
+++ src/Frame.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react';
 
+import { connect } from 'react-redux';
+
 import './Frame.css';
 
-export class Frame extends React.Component {
+interface IStateProps {
+  course?: any
+  submission?: any
+  username?: string
+}
+
+export class Frame extends React.Component<IStateProps> {
 
   public componentDidMount() {
     window.scrollTo(0, 0)
   }
 
   public render() {
-    const { children } = this.props
+    const { children, username } = this.props
 
     return (
       <div className="frame">
+        <div className="headerbar">
+          <span className="logo">{username ? 'Hey ' + username + ', ben jij een Struisvogel?' : 'Ben jij een Struisvogel?'}</span>
+        </div>
         <div className="frame_container">
           {children}
         </div>
@@ -21,4 +32,12 @@ export class Frame extends React.Component {
   }
 }
 
-export default Frame
+function mapStateToProps(state: any, ownProps: any): IStateProps {
+  return {
+    course: state.course.course,
+    submission: state.session.courseSubmissions,
+    username: state.session.username,
+  }
+}
+
+export default connect(mapStateToProps)(Frame)
